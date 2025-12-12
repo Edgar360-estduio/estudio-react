dd437d76406505d91c0eca05072c09ea1e517786
Edgar Cortes Flores <edgar.cortes@mosmex.local>
2025-12-12 09:25:53 -0600

    Mostrar fondo en Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import "./styles/global.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
+import bg from "./assets/Imagenes/SESION_IMG_(38).jpg"; // ← importa tu imagen
+
+
 export default function App() {
   return (
     <>
       <Header />
-      <main>
+      <main
+      style={{
+          backgroundImage: `url(${bg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+          paddingTop: "80px",
+        }}
+      >
         <Hero />
       </main>
 
