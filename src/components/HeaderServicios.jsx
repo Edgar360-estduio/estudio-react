2255f89a05436ba35b319cbf5e1c76554266e9af
Edgar Cortes Flores <edgar.cortes@mosmex.local>
2025-12-12 13:44:27 -0600

    Se modifica imagen

--- src/components/HeaderServicios.jsx
+++ src/components/HeaderServicios.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/Imagenes/LOGO_360.png";
+import logo from "../assets/Imagenes/LOGO_360.jpg";
 
 export default function HeaderServicios() {
   return (
